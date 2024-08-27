775888e77e908f2c1e2f837311455a64ad7a2ec2
shahviraj222 <viraj22feb@gmail.com>
2024-08-27 12:47:03 +0530

    listening request

--- 03PixelCast/src/index.js
+++ 03PixelCast/src/index.js
@@ -1,7 +1,22 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
+import { app } from './app.js'
+
 // dotenv.config()
+
+const port = process.env.PORT || 8000
 dotenv.config({
     path: '.env'
 })
 connectDB()
+    .then(() => {
+        app.on("error", (err) => {
+            console.log("Middleware Error:", err)
+        })
+        app.listen(port, () => {
+            console.log("Server is runing at PORT : ", port)
+        })
+    })
+    .catch((err) => {
+        console.log("Error:", err)
+    })
