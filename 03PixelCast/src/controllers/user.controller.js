b54ca82c4e50b0eacb32fc0b9f892e2db1175732
shahviraj222 <viraj22feb@gmail.com>
2024-09-16 16:37:40 +0530

    syntax error

--- 03PixelCast/src/controllers/user.controller.js
+++ 03PixelCast/src/controllers/user.controller.js
@@ -46,7 +46,7 @@ const registerUser = asyncHandler(async (req, res) => {
     })
 
     if (existedUser) {
-        ApiError(409, "User already exist with username and email")
+        throw new ApiError(409, "User already exist with username and email")
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
@@ -102,7 +102,7 @@ const loginUser = asyncHandler(async (req, res) => {
     // 4 Send Tokens In Cookies
 
     const { email, username, password } = req.body
-    if (!username || !email) {
+    if (!username && !email) {
         throw new ApiError("username or email required")
     }
 
