4d228daf9c63b1c47c1340acc4f37827202b9e27
shahviraj222 <viraj22feb@gmail.com>
2024-10-05 12:46:41 +0530

    getting subscriber,to subscribed

--- 03PixelCast/src/controllers/user.controller.js
+++ 03PixelCast/src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from '../models/user.model.js'
 import { uploadOnCloudinary } from '../utils/cloudinary.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import jwt, { decode } from 'jsonwebtoken'
+import mongoose from 'mongoose'
 
 
 const generateAccessAndRefreshTokens = async (userId) => {
@@ -350,4 +351,103 @@ const udpateUserCoverImage = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, user, "CoverImage Updated Successfully"))
 })
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetail, updateUserAvatar, udpateUserCoverImage } 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "username is missing ")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        // finding who is subscribe to me..       
+        {
+            $lookup: {
+                from: "subscriptions",   // in database all document names are in lower case and it become pular
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        // finding to whom I am subscribed    
+        {
+            $lookup: {
+                from: "subscriptions",   // in database all document names are in lower case and it become pular
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        // this add the field in the user object that return (first one)
+        {
+            $addFields: {
+                subscribersCount: {
+                    // size count the number in the field
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+
+                    $cond: {
+                        // if , then , else
+                        // in operator is look in Array , Objects 
+                        if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        // which value you want to pass
+        {
+            $project: {
+                fullname: 1,
+                username: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1
+            }
+        }
+    ])
+
+    if (!channel?.length) {
+        throw new ApiError(400, "channel does not exists")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, channel[0], "User Channel Fetched Successfully"))
+})
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+
+    // const id = req.user._id  //we only get the string not actual id
+    // User.findbyId(id)
+    // here the findbyId is gone by mongoose it will take care of the id convet into mongoose id
+    // all aggreation code go as it is ! So we have to take care 
+
+    const user = await User.aggregate([
+        {
+            $match: {
+                // _id: new mongoose.Types.ObjectId(req.user._id)  //deprecated
+                // _id:mongoose.Types.ObjectId(req.user._id)       // some time give error
+                _id: new ObjectId(product_id)                     // safest
+            }
+        }
+    ])
+
+})
+export {
+    registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetail, updateUserAvatar, udpateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+} 
