4565d37d0c6ed2c2000688be6a3e8a7537d1e193
shahviraj222 <viraj22feb@gmail.com>
2024-10-05 17:16:11 +0530

    added routers

--- 03PixelCast/src/routes/user.routes.js
+++ 03PixelCast/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetail, updateUserAvatar, udpateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -18,11 +18,47 @@ router.route('/register').post(
     ]),
     registerUser
 )
-router.route('/login').post(loginUser)
+
+router
+    .route('/login')
+    .post(loginUser)
 
 // secured routes 
 // here the verifyJWT work first and then logoutuser run 
-router.route('/logout').post(verifyJWT, logoutUser)
-router.route('/refresh-token').post(refreshAccessToken)
+router
+    .route('/logout')
+    .post(verifyJWT, logoutUser)
+
+router
+    .route('/refresh-token')
+    .post(refreshAccessToken)
+
+router
+    .route('/change-password')
+    .post(verifyJWT, changeCurrentPassword)
+
+router
+    .route('/current-user')
+    .get(verifyJWT, getCurrentUser)
+
+router
+    .route('/update-account')
+    .patch(verifyJWT, updateAccountDetail)
+
+router
+    .route('/update-avatar')
+    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+router
+    .route('/update-cover-image')
+    .patch(verifyJWT, upload.single("/coverImage"), udpateUserCoverImage)
+
+router
+    .route('/c/:username')
+    .get(verifyJWT, getUserChannelProfile)
+
+router
+    .route('/history')
+    .get(verifyJWT, getWatchHistory)
 
 export default router 
